b49a3a31eb057108c03508c7c0362ffe5fb5cea1
Magdalena Noffke <noffke@mi.fu-berlin.de>
2014-10-25 16:29:50 +0200

    some changes on app

--- app1.js
+++ app1.js
@@ -1,23 +1,30 @@
 $( document ).ready(function() {
 
-  var spaceCircles = [30, 70, 110];
-
-  
-
   var svgContainer = d3.select("body").append("svg")
-                                      .attr("width", 200)
-                                      .attr("height", 200)
+                                      .attr("width", 700)
+                                      .attr("height", 700)
                                       .style("border", "1px solid black");
 
-  var circles = svgContainer.selectAll("circle")
-                            .data(spaceCircles)
+
+  d3.json("data/orion.json", function(error, stars) {
+
+    var circles = svgContainer.selectAll("circle")
+                            .data(stars)
                             .enter()
                             .append("circle");
 
-  var circleAttributes = circles
-                       .attr("cx", function (d) { return d; })
-                       .attr("cy", function (d) { return d; })
-                       .attr("r", 20 );
+    var circleAttributes = circles
+                       .attr("cx", function (d) { return d.x; })
+                       .attr("cy", function (d) { return d.y; })
+                       .attr("name", function (d) { return d.name; })
+                       .attr("r", 5 );
+
+
+    
+  });
+  
+
+  
 
 
 });
